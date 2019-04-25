e87acce655f47f3954b625233487529a3a25cd66
Compagnoni <ac22qawo@idiv.de>
2019-04-25 13:09:06 +0200

    Add histograms of anomalies

--- analysis/get_climate/anomalies_vs_year.R
+++ analysis/get_climate/anomalies_vs_year.R
@@ -6,6 +6,7 @@ library(rstan)
 library(rstanarm)
 library(lme4)
 library(bbmle)
+library(gridExtra)
 options(stringsAsFactors = F)
 source("analysis/format_data/format_scripts.R")
 source("analysis/format_data/format_functions.R")
@@ -19,7 +20,7 @@ clim        <- read.csv( "data/prism_point_reyes_87_18.csv")
 # climate format ----------------------------------------------------------------
 lupine_df$transition
 # 4.19.2018, 2019 data not yet in
-years     <- 2006:2018
+years     <- 1990:2018
 m_obs     <- 5
 m_back    <- 36
 
@@ -57,6 +58,7 @@ clim_mat <- Reduce( function(...) full_join(...),
                     list(ppt_mat,tmp_mat,enso_mat) ) %>% 
               gather(measure, anomaly, ppt_t0:oni_t0_tm2) 
 
+
 # climate anomalies by year ------------------------
 clim_mat %>% 
   subset( grepl('_t0$',measure) ) %>% 
@@ -67,8 +69,60 @@ clim_mat %>%
   scale_color_viridis_d() +
   ylab( 'Anomaly' ) + 
   xlab( 'Year' ) +
+  geom_vline( xintercept = 2006,
+              lty  = 2 ) + 
   theme( axis.title = element_text( size = 20),
-         axis.text  = element_text( size = 20) ) + 
+         axis.text  = element_text( size  = 20,
+                                    angle = 90) ) + 
   ggsave('results/climate/anomalies_vs_year.tiff',
-         width=6.3,height=6.3,compression='lzw')
+         width=6.3,height=4,compression='lzw')
   
+
+# histograms of anomalies -------------------------------------
+p1 <- clim_mat %>%
+        subset( grepl('_t0$',measure) ) %>% 
+        ggplot( ) + 
+        geom_histogram( aes(anomaly),
+                        binwidth = 3 ) + 
+        facet_grid( ~ measure) + 
+        xlab( '' ) +
+        ylab( '' ) +
+        theme(plot.margin = unit(c(0,0,0,0), "cm"),
+              panel.spacing = unit('0.5',unit='mm'),
+              strip.text.x  = element_text( size = 10,
+                                       margin = margin(0,0,0,0,
+                                                       'mm') ) )
+  
+p2 <- clim_mat %>%
+        subset( grepl('_t0$',measure) ) %>%
+        subset( year > 2005 ) %>% 
+        ggplot( ) + 
+        geom_histogram( aes(anomaly),
+                        binwidth = 3 ) + 
+        facet_grid( ~ measure) + 
+        xlab( '' ) + 
+        theme(plot.margin = unit(c(0,0,0,0), "cm"),
+              panel.spacing = unit('0.5',unit='mm'),
+              strip.text.x  = element_text( size = 10,
+                                       margin = margin(0,0,0,0,
+                                                       'mm') ) )
+
+p3 <- clim_mat %>%
+        subset( grepl('_t0$',measure) ) %>%
+        subset( year < 2006 ) %>% 
+        ggplot( ) + 
+        geom_histogram( aes(anomaly),
+                        binwidth = 3 ) + 
+        facet_grid( ~ measure) +
+        ylab( '' ) +
+        theme(plot.margin = unit(c(0,0,0,0), "cm"),
+              panel.spacing = unit('0.5',unit='mm'),
+              strip.text.x  = element_text( size = 10,
+                                       margin = margin(0,0,0,0,
+                                                       'mm') ) )
+
+g <- arrangeGrob(p1,p2,p3,nrow=3,ncol=1)
+ggsave(filename = 'results/climate/anom_hist.tiff',
+       plot = g,
+       dpi = 300, width = 6.3, height = 4, units = "in",
+       compression = 'lzw')
