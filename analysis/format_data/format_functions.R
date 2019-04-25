9dd44c1683727eec7a76f0ca9ad338d9bb682e77
Compagnoni <ac22qawo@idiv.de>
2019-04-25 13:15:06 +0200

    Add spei to data and formatting funs

--- analysis/format_data/format_functions.R
+++ analysis/format_data/format_functions.R
@@ -171,6 +171,48 @@ prism_clim_form <- function(clim_x, clim_var = "ppt", yearz, m_back, m_obs){
 
 }
 
+# format spei data
+spei_clim_form <- function(clim_x, yearz, m_back, m_obs){
+  
+  # "spread" the 12 months
+  clim_m <- clim_x %>%  
+              select( -clim_var ) %>% 
+              spread( month_num, clim_value)
+  
+  clim_detr <- clim_m
+  yr_range  <- range(yearz)
+  
+  # detrended climate in "long" form
+  long_out  <- clim_detr %>%
+                  subset(year < (yr_range[2]+1) & year > (yr_range[1] - 6) ) %>%
+                  gather('month', 'clim_value', '1':'12') %>%
+                  setNames(c("year", "month_num", "clim_value") ) %>% 
+                  mutate(month_num = factor(month_num, levels = paste0(1:12)) ) %>% 
+                  arrange(year, month_num)
+  
+  # select temporal extent
+  clim_back <- function(yrz, m_obs, dat){
+    id <- which(dat$year == yrz & dat$month_num == m_obs)
+    r  <- c( id:(id - (m_back-1)) )
+    return(dat[r,"clim_value"])
+  }
+  
+  # climate data in matrix form 
+  year_by_month_mat <- function(dat, years){
+    do.call(rbind, dat) %>% 
+      as.data.frame %>%
+      tibble::add_column(year = years, .before=1)
+  }
+  
+  # calculate monthly precipitation values
+  clim_x_l  <- lapply(yearz, clim_back, m_obs, long_out)
+  x_clim    <- year_by_month_mat(clim_x_l, yearz)
+  return(x_clim)
+
+}
+
+
+
 
 # detrend population-level climate; put it in "long" form
 month_clim_form <- function(clim_x, clim_var = "precip", yearz, m_back, m_obs){
--- analysis/format_data/format_prism.R
+++ analysis/format_data/format_prism.R
@@ -1,10 +1,12 @@
 # Produce a dataframe of climate predictors
-setwd("C:/cloud/Dropbox/lupine")
+# 1. format prism data 
+# 2. calculate SPEI (IMPORTANT: pay attention to missing months,
+#    at end of climate time series)
 library(dplyr)
 library(tidyr)
 options(stringsAsFactors = F)
 
-# format prism data -------------------------------------------------------------------------
+#1. format prism data -------------------------------------------------------------------------
 prism_f   <- paste0('data/weather_station/prism/', list.files('data/weather_station/prism') )
 prism_l   <- lapply(prism_f, read.csv) %>% bind_rows
 yr_prism  <- prism_l$year %>% unique %>% .[-c(1:4,32)]
@@ -15,7 +17,47 @@ prism_df  <- prism_l %>%
                         clim_var   = variable ) %>% 
                 select( year,month_num, clim_var, clim_value)  
 
+# add 12-month SPEI to data ----------------------------------
+
+# create placeholder for calculations
+pr_clim     <- prism_df %>% 
+                 rename( MONTH = month_num ) %>% 
+                 spread( clim_var, clim_value)
+
+# Compute potential evapotranspiration (PET) and climatic water balance (BAL)
+pr_clim$PET <- thornthwaite(pr_clim$tmean, 38.06667)
+pr_clim$BAL <- pr_clim$ppt - pr_clim$PET
+
+# transform in 
+pr_ts <- ts(pr_clim[,-c(1,2)], 
+            end=c(2018,6), 
+            frequency=12)
+
+# calculate SPEI
+pr_spei12 <- spei(pr_ts[,'BAL'], 12)
+
+# format only months with complete data (1987-2017)
+spei_df   <- matrix(pr_spei12$fitted[1:(12*31)],
+               nrow=31,ncol=12,
+               byrow=T) %>% 
+              # IMPORTANT: pay attention to missing months,
+              # at end of climate time series
+              rbind( c(pr_spei12$fitted[((12*31)+1):((12*31)+6)],
+                       rep(NA,6) ) ) %>% 
+              as.data.frame %>% 
+              # add column of years (link to prism_df)
+              tibble::add_column( year = prism_df$year %>% unique %>% sort,
+                                  .before=1 ) %>% 
+              gather( month_num, clim_value, V1:V12 ) %>% 
+              mutate( month_num = gsub('V','',month_num) ) %>% 
+              mutate( month_num = as.numeric(month_num) ) %>% 
+              tibble::add_column(clim_var = 'spei',
+                                 .before  = 3) %>% 
+              arrange( year, month_num) %>% 
+              # remove NAs
+              subset( !is.na(clim_value ) )
+        
 # write out formatted file
-write.csv(prism_df,
-          'C:/cloud/Dropbox/lupine/data/prism_point_reyes_87_18.csv',
+write.csv(bind_rows(prism_df, spei_df),
+          'data/prism_point_reyes_87_18.csv',
           row.names=F)
