51e3fb59850c379982f8637f144edbc763298821
Compagnoni <ac22qawo@idiv.de>
2019-05-16 15:16:51 +0200

    Crossvalidation climatic predictors: first commit

---
+++ analysis/vital_rates/crossvalidation/fert_cv_t0.R
@@ -0,0 +1,252 @@
+rm(list=ls())
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(testthat)
+library(bbmle)
+library(lme4)
+library(readxl)
+options(stringsAsFactors = F)
+source("analysis/format_data/format_scripts.R")
+source("analysis/format_data/format_functions.R")
+
+# data
+lupine_df   <- read.csv( "data/lupine_all.csv")
+enso        <- read.csv( "data/enso_data.csv")
+clim        <- read.csv( "data/prism_point_reyes_87_18.csv")
+fruit_rac   <- read_xlsx('data/fruits_per_raceme.xlsx')
+seed_x_fr   <- read_xlsx('data/seedsperfruit.xlsx')
+germ        <- read_xlsx('data/seedbaskets.xlsx')
+
+
+
+# data format --------------------------------------------------------------
+fert        <- subset(lupine_df, flow_t0 == 1 ) %>% 
+                  subset( area_t0 != 0) %>% 
+                  subset( !is.na(numrac_t0) ) %>% 
+                  # remove non-flowering individuals
+                  subset( !(flow_t0 %in% 0) ) %>% 
+                  mutate( log_area_t0  = log_area_t0_z,
+                          log_area_t02 = log_area_t0_z^2 ) %>% 
+                  # remove zero fertility (becase fertility should not be 0)
+                  # NOTE: in many cases, notab_t1 == 0, because numab_t1 == 0 also
+                  subset( !(numrac_t0 %in% 0) ) 
+
+# climate format ----------------------------------------------------------------
+years     <- 1990:2018 #unique(fert$year)
+m_obs     <- 5
+m_back    <- 36
+
+# calculate yearly anomalies from monthly anomalies
+year_m_anom <- function(x, var ){
+
+  # set names of climate variables
+  clim_names <- paste0( var,c('_t0','_tm1','_t0_tm1','_t0_tm2') )
+
+  mutate(x,
+         avgt0     = x %>% select(V1:V12) %>% rowSums,
+         avgtm1    = x %>% select(V13:V24) %>% rowSums,
+         avgt0_tm1 = x %>% select(V1:V24) %>% rowSums,
+         avgt0_tm2 = x %>% select(V1:V36) %>% rowSums ) %>%
+    select(year, avgt0, avgtm1, avgt0_tm1, avgt0_tm2) %>%
+    setNames( c('year',clim_names) )
+
+}
+
+# calculate yearly anomalies
+year_anom <- function(clim_x, clim_var = "ppt", 
+                      years, m_back, m_obs ){
+  
+  # "spread" the 12 months
+  clim_m <- select(clim_x, -clim_var )
+  
+  # select temporal extent
+  clim_back <- function(yrz, m_obs, dat){
+    id <- which(dat$year == yrz & dat$month_num == m_obs)
+    r  <- c( id:(id - (m_back-1)) )
+    return(dat[r,"clim_value"])
+  }
+  
+  # climate data in matrix form 
+  year_by_month_mat <- function(dat, years){
+    do.call(rbind, dat) %>% 
+      as.data.frame %>%
+      tibble::add_column(year = years, .before=1)
+  }
+  
+  # calculate monthly precipitation values
+  clim_x_l  <- lapply(years, clim_back, m_obs, clim_m)
+  x_clim    <- year_by_month_mat(clim_x_l, years) %>% 
+                  gather(month,t0,V1:V12) %>% 
+                  select(year,month,t0) %>% 
+                  mutate( month = gsub('V','',month) ) %>%
+                  mutate( month = as.numeric(month) )
+  
+  if( clim_var == 'ppt'){
+    raw_df <- x_clim %>% 
+                group_by(year) %>% 
+                summarise( ppt_t0 = sum(t0) ) %>% 
+                ungroup %>% 
+                arrange( year ) %>% 
+                mutate( ppt_t0  = scale(ppt_t0)[,1] ) %>% 
+                mutate( ppt_tm1 = lag(ppt_t0) )
+  }
+  if( clim_var == 'tmp'){
+    raw_df <- x_clim %>% 
+                group_by(year) %>% 
+                summarise( tmp_t0 = mean(t0) ) %>% 
+                ungroup %>% 
+                arrange( year ) %>% 
+                mutate( tmp_t0  = scale(tmp_t0)[,1] ) %>% 
+                mutate( tmp_tm1 = lag(tmp_t0) )
+  }
+  
+  raw_df
+  
+}
+
+
+# format climate - need to select climate predictor first 
+ppt_mat <- subset(clim, clim_var == "ppt") %>%
+              year_anom("ppt", years, m_back, m_obs) #%>% 
+              # prism_clim_form("precip", years, m_back, m_obs) %>% 
+              # year_anom('ppt')
+
+tmp_mat <- subset(clim, clim_var == 'tmean') %>% 
+              year_anom("tmp", years, m_back, m_obs)
+              # prism_clim_form('tmean', years, m_back, m_obs) %>% 
+              # year_anom('tmp')
+  
+enso_mat <- subset(enso, clim_var == 'oni' ) %>%
+              month_clim_form('oni', years, m_back, m_obs) %>% 
+              year_m_anom('oni')
+
+spei_mat <- subset(clim, clim_var == 'spei' ) %>%
+              spei_clim_form(years, m_back, m_obs) %>% 
+              year_m_anom('spei')
+
+# put together all climate
+clim_mat <- Reduce( function(...) full_join(...),
+                    list(ppt_mat, tmp_mat, enso_mat, spei_mat) )
+
+# demography plus clim
+fert_clim  <- left_join(fert, clim_mat) %>%
+                subset( !is.na(location) )
+
+
+# climate model selection ---------------------------------------
+climate_mods <- list(
+
+  # null
+  numrac_t0 ~ log_area_t0 + log_area_t02 + (log_area_t0 | year) + (log_area_t0 | location),
+  
+  # precipitation
+  numrac_t0 ~ log_area_t0 + log_area_t02 + ppt_t0 + (log_area_t0 | year) + (log_area_t0 | location),
+  numrac_t0 ~ log_area_t0 + log_area_t02 + ppt_tm1 + (log_area_t0 | year) + (log_area_t0 | location),
+  
+  # temperature
+  numrac_t0 ~ log_area_t0 + log_area_t02 + tmp_t0 + (log_area_t0 | year) + (log_area_t0 | location),
+  numrac_t0 ~ log_area_t0 + log_area_t02 + tmp_tm1 + (log_area_t0 | year) + (log_area_t0 | location),
+  
+  # enso
+  numrac_t0 ~ log_area_t0 + log_area_t02 + oni_t0 + (log_area_t0 | year) + (log_area_t0 | location),
+  numrac_t0 ~ log_area_t0 + log_area_t02 + oni_tm1 + (log_area_t0 | year) + (log_area_t0 | location),
+
+  # spei
+  numrac_t0 ~ log_area_t0 + log_area_t02 + spei_t0 + (log_area_t0 | year) + (log_area_t0 | location),
+  numrac_t0 ~ log_area_t0 + log_area_t02 + spei_tm1 + (log_area_t0 | year) + (log_area_t0 | location)
+
+)
+
+# years to do crossvalidation on
+years_cv <- unique(fert_clim$year)
+
+# function to carry out crossvalidation
+crossval <- function(yr_ii){ #length(years_cv)
+  
+  # set up dataframes to train and test year-specific predictions
+  train_df <- subset(fert_clim, !(year %in% yr_ii ) )
+  test_df  <- subset(fert_clim,   year == yr_ii ) %>% 
+                mutate( resp = numrac_t0 )
+
+  # fit all models
+  mods_clim <- lapply( climate_mods, 
+                         function(x) glmer(x, data=train_df, family='poisson') ) %>% 
+                    setNames( c( 'null',
+                                 'ppt_t0',  'ppt_tm1', 
+                                 'tmp_t0',  'tmp_tm1',
+                                 'oni_t0',  'oni_tm1',
+                                 'spei_to', 'spei_tm1') )
+  
+  mods_brier <- list()
+  for(ii in 1:length(mods_clim)){
+    
+    mods_brier[[ii]] <- test_df %>% 
+                          # calculate predictions
+                          mutate( pred = predict( mods_clim[[ii]], 
+                                                  newdata = test_df,
+                                                  type    = 'response',
+                                                  re.form = NA ) 
+                                ) %>% 
+                          # brier's score
+                          # mutate( brier = -2*dpois(resp,lambda=pred) + 
+                          #                 sapply(pred, 
+                          #                        function(x){ sum(dpois(1:1000,
+                          #                                               lambda=x)^2) })
+                          #           
+                          #        ) %>% 
+                          mutate( brier = (resp - pred)^2 ) %>% 
+                          .$brier
+    
+  }
+  
+  mods_brier %>% 
+    setNames( names(mods_clim) ) %>% 
+    as.data.frame 
+  
+}
+
+
+# detect cores
+cores     <- 3 #detectCores()
+# set up as many clusters as detected by 'detectCores()'
+cluster   <- parallel::makePSOCKcluster(cores)
+
+# attach packages that will be needed on each cluster
+clusterEvalQ(cluster, list(library(lme4), library(boot),  
+                           library(dplyr) , library(tidyr)) )
+
+# attach objects that will be needed on each cluster
+clusterExport(cluster, c('fert_clim', 'climate_mods') )
+
+# Fit 5 alternative models on 200 bootstrapped samples
+init_t <- Sys.time()
+boostr_par_l  <- parLapply(cluster, years_cv, crossval) 
+Sys.time() - init_t 
+
+# lapply( function(x) setNames(x, c('null',
+#                                   'ppt_t0',  'ppt_tm1', 
+#                                   'tmp_t0',  'tmp_tm1',
+#                                   'oni_t0',  'oni_tm1',
+#                                   'spei_to', 'spei_tm1') ) )
+
+
+boostr_par_l <- lapply(boostr_par_l, function(x) setNames(x, c('null',
+                                                     'ppt_t0',  'ppt_tm1', 
+                                                     'tmp_t0',  'tmp_tm1',
+                                                     'oni_t0',  'oni_tm1',
+                                                     'spei_to', 'spei_tm1') ) )
+
+# get scores
+score_df <- Reduce(function(...) rbind(...), boostr_par_l) %>% 
+              # take the mean of the squared residuals
+              colMeans %>% 
+              as.data.frame %>% 
+              tibble::add_column(model = row.names(.), .before=1) %>% 
+              rename( score = "." ) %>% 
+              arrange( score ) %>% 
+              mutate( dScore = score - score[1] )
+
+write.csv(score_df,
+          'results/vital_rates/crossval/fert_cv_mse.csv',
+          row.names=F)
