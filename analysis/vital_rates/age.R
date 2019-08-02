2967c546edbac76d0f53c00c1f6272f34b108f85
Compagnoni <ac22qawo@idiv.de>
2019-08-02 12:05:06 -0500

    Update average ages at death, and make histograms of it

--- analysis/vital_rates/age.R
+++ analysis/vital_rates/age.R
@@ -1,23 +1,40 @@
 # calculate average age at death
 # calculate average age at death for non seedlings
+library(dplyr)
 
 # read data
 lupine_df   <- read.csv( "data/lupine_all.csv")
 
 # average age at death
-trans_d %>% 
+lupine_df %>% 
   # remove data prior to 2012
   # (prop. of population with age info below 75%)
   subset( year > 2011 ) %>% 
   subset( !is.na(age_t0) ) %>% 
   # subset( age_t0 > 1 ) %>% 
-  .$age_t0 %>% mean
+  .$age_t0 %>% 
+  mean
 
 # average age at death of adults (past 1 year of age)
-trans_d %>% 
+lupine_df %>% 
   # remove data prior to 2012
   # (prop. of population with age info below 75%)
-  subset( year > 2011 ) %>% 
+  subset( year > 2007 ) %>% 
   subset( !is.na(age_t0) ) %>% 
-  subset( age_t0 > 1 ) %>%
-  .$age_t0 %>% mean
+  subset( age_t0 > 2 ) %>% 
+  .$age_t0 %>% 
+  mean
+
+
+# histogram of dying individuals
+age_at_death <- lupine_df %>% 
+  # remove data prior to 2012
+  # (prop. of population with age info below 75%)
+  subset( year > 2007 ) %>% 
+  subset( !is.na(age_t0) ) %>% 
+  subset( surv_t1 == 0 ) %>% 
+  .$age_t0 
+
+# proportion of seeds dying at age...
+tab_d <- age_at_death %>% table
+(tab_d / sum(tab_d)) %>% round(3)
